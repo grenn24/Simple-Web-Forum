30450806a0ed059d51f09749a2e0f5d45b215bed
Hoo Di Heng <Hoodiheng@gmail.com>
2024-12-15 11:55:22 +0800

    added background image

--- frontend/src/pages/WelcomeScreen.tsx
+++ frontend/src/pages/WelcomeScreen.tsx
@@ -1,11 +1,18 @@
 import { Box } from "@mui/material";
-import whiteBackground from "../assets/images/white-background.jpg";
-
+import whiteBackground from "../assets/image/white-background.jpg";
 
 const WelcomeScreen = () => {
-	return <Box sx={{backgroundImage: whiteBackground}}>
-
-	</Box>;
+	return (
+		<Box
+			sx={{
+				backgroundImage:
+					`url(${whiteBackground})`,
+				backgroundSize: "cover",
+			}}
+			width="100vw"
+			height="100vh"
+		></Box>
+	);
 };
 
 export default WelcomeScreen;
